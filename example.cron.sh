a4871751b4b2ebb1e40d4540fc99fdbd02c92944
Jacob Friis Saxberg <jacob@webcom.dk>
2023-12-10 15:25:18 +0100

    fix: invalid loads value

--- example.cron.sh
+++ example.cron.sh
@@ -4,7 +4,7 @@ HEADERS='{"X-API-Key": "xyz"}'
 STARTPOINT="https://api.example.com/start"
 ENDPOINT="https://api.example.com/end"
 PASSWD=$(<$HOME/.haypass)
-CMD="./main.py --headers $HEADERS --startpoint $STARTPOINT --endpoint $ENDPOINT --key $PASSWD"
+CMD="./main.py --headers '$HEADERS' --startpoint $STARTPOINT --endpoint $ENDPOINT --key $PASSWD --verbose"
 
 export USER_AGENT_COMMENT="I am @webjay"
 export PATH=/opt/homebrew/bin:/usr/bin:/usr/local/bin:$PATH
