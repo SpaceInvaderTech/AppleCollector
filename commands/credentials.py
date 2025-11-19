2360b14e1f7f7d5b4f0cc69dec7d67e2ae32d01f
Christophoros <bekos.christopher@gmail.com>
2025-11-20 00:03:17 +0200

    fix typo

--- commands/credentials.py
+++ commands/credentials.py
@@ -1,25 +1,26 @@
 from app.haystack import get_headers
 from app.credentials.api import api_credentials_service
+from app.models import ICloudCredentials
 from app.settings import settings
 import logging
 
 logger = logging.getLogger(__name__)
 
-security_headers = get_headers(decryption_key=settings.PASSWD)
-logger.info("Security headers generated successfully.")
-api_credentials_service.update_credentials(security_headers, api_key=settings.CREDENTIALS_API_KEY)
-logger.info("Security headers updated successfully.")
-
-
 def refresh_credentials_on_aws(schedule_location_fetching: bool = True):
     """
     This function must be invoked on a MacOS device to refresh the credentials.
     """
-    security_headers = get_headers(decryption_key=settings.PASSWD)
+    _security_headers = get_headers(decryption_key=settings.PASSWD)
     logger.info("Security headers generated successfully.")
 
     api_credentials_service.update_credentials(
-        headers=security_headers,
-        api_key=settings.CREDENTIALS_API_KEY,
+        credentials=ICloudCredentials(**_security_headers),
         schedule_data_fetching=schedule_location_fetching)
+    logger.info("Security headers updated successfully.")
+
+
+if __name__ == "__main__":
+    security_headers = get_headers(decryption_key=settings.PASSWD)
+    logger.info("Security headers generated successfully.")
+    api_credentials_service.update_credentials(ICloudCredentials(**security_headers))
     logger.info("Security headers updated successfully.")
